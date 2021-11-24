b884fb1e2c68d352027fa0d88b516ccc94ea85a0
Timothy Shava <ttshava@gmail.com>
2021-11-24 06:43:13 -0800

    Production
    Production environment working

--- YEMS/settings.py
+++ YEMS/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-%l414)117f-_#m*o&a4j%0n@5ay#-!1kblr$=7y=4f=8%=@2py
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['youthdrive.herokuapp.com','127.0.0.0']
 
 
 # Application definition
